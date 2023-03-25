fd3e21ef2397a4f2413a23c645a8e45a5ea31f11
sound-round <yudaev1@gmail.com>
2023-03-25 14:37:36 +0300

    Connect to OPENAI API

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import logging
 import telegram
+import httpx
 
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
 from telegram.error import NetworkError
 from telegram.ext import ApplicationBuilder, MessageHandler, filters
 from pydantic import BaseModel
 
 from enviroment import TELEGRAM_TOKEN
-from utils import send_request
+from openai_client import OpenAIClient
+
+
+API_client = OpenAIClient()
 
 
 logging.basicConfig(
@@ -21,14 +25,18 @@ application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
 
 async def handle_message(update, context):
-    print("\033[91m HERE \033[0m")
     message = update.message.text
-    # try:
-    #     response = await send_request(message)
-    # except NetworkError as exc:
-    #     print("\033[91m ERROR: \033[0m", exc)
-
-    response = "Hello, I'm your bot"
+    try:
+        response = await API_client.send_request(message)
+    except httpx.ConnectTimeout as exc:
+        print("\033[91m ERROR: connection timed out \033[0m")
+        response = f"exception: connection timed out"
+    except (NetworkError, httpx.HTTPError) as exc:
+        print("\033[91m ERROR: \033[0m", exc)
+        response = f"exception: {exc}"
+    except Exception as exc:
+        print("\033[91m COMMON EXCEPTION: \033[0m", exc)
+        response = f"exception: {exc}"
 
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
@@ -36,9 +44,12 @@ handler = MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
 
 
 if __name__ == '__main__':
-
     application.add_handler(handler)
     application.run_polling()
 
 
-# TODO: Add inline mode
+# TODO:
+# resolve problem on running
+# context
+# commands
+# inline mode
---
+++ openai_client.py
@@ -0,0 +1,54 @@
+import httpx
+
+from enviroment import OPENAI_TOKEN
+
+
+CONFIG = {
+    "model": 'gpt-3.5-turbo',
+    'max_tokens': 500,
+    'temperature': 0.5,
+    'n': 1,
+    # 'stop': '.\n',
+}
+HEADERS = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {OPENAI_TOKEN}',
+}
+ROLE = 'user'
+TIMEOUT = 30
+URL = 'https://api.openai.com/v1/chat/completions'
+
+SYSTEM_PROMPT = "You are a helpful assistant. Your name is Alfred"
+
+
+class OpenAIClient:
+    def __init__(
+        self,
+        url: str = URL,
+        headers: dict[str, str] = HEADERS,
+        timeout: int = TIMEOUT,
+        config=CONFIG,
+
+    ):
+        self.url = url
+        self.headers = headers
+        self.timeout = timeout
+        self.config = config
+
+    async def send_request(self, text):
+        system_prompt = {'role': "system", 'content': SYSTEM_PROMPT}
+        user_message = {'role': "user", 'content': text}
+        messages = [system_prompt, user_message]
+        request_params = dict(messages=messages, **self.config)
+
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                self.url,
+                headers=self.headers,
+                json=request_params,
+                timeout=self.timeout,
+            )
+
+            response.raise_for_status()
+            response_json = response.json()
+            return response_json['choices'][0]['message']['content']
