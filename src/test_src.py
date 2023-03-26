b39377b8df412f9fa1f931e17c33d729017c8aae
sound-round <yudaev1@gmail.com>
2023-03-26 16:47:12 +0300

    Add tokenizer

---
+++ src/test_src.py
@@ -0,0 +1,10 @@
+from .tokenizer import tokenize
+
+def test_tokenizer():
+    tokens = tokenize("Some text")
+    assert tokens
+    assert tokens == 2
+    
+    tokens = tokenize("Thisisthelongwordwithagreatnumberofcharacters")
+    assert tokens
+    assert tokens == 6
---
+++ src/tokenizer.py
@@ -0,0 +1,12 @@
+TOKEN_K = 4
+WORD_K = 0.75
+
+
+def tokenize(text: str):
+    char_count = len(text)
+    word_count = len(text.split())
+
+    tokens_by_chars = int(char_count / TOKEN_K)
+    tokens_by_words = int(word_count / WORD_K)
+    
+    return int((tokens_by_chars + tokens_by_words) / 2)
